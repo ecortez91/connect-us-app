ce031f104c5b6445fb477024b6d00ad1da6d3245
Eduardo Cortez <Eduardo.Cortez@Focusservices.com>
2023-11-21 15:44:22 -0600

    Adding routing and redux

--- connect-us-frontend/src/App.js
+++ connect-us-frontend/src/App.js
@@ -1,15 +1,29 @@
 import { useEffect } from 'react';
+import {
+  BrowserRouter as  Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
 import { connectWithWebSocket } from './utils/wssConnection/wssConnection';
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();    
    }, []);
   return (
-    <div className="App">
-      Hello react app
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/dashboard'>
+          <Dashboard />
+        </Route>
+        <Route path='/'>
+          <LoginPage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
