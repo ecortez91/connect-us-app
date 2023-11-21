f9280fd506d74aa03332c57186fb91f7f3faeaab
Eduardo Cortez <Eduardo.Cortez@Focusservices.com>
2023-11-21 15:15:06 -0600

    Connecting frontend with backend with socket.io

--- connect-us-frontend/src/App.js
+++ connect-us-frontend/src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import './App.css';
+import { connectWithWebSocket } from './utils/wssConnection/wssConnection';
 
 function App() {
+  useEffect(() => {
+    connectWithWebSocket();    
+   }, []);
   return (
     <div className="App">
       Hello react app
